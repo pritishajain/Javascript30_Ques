1c0c72e72600fdf74d4db82c5f5623eafb569501
pj <pritishajain05@gmail.com>
2024-01-29 15:03:10 +0530

    flex panels image gallery

---
+++ FlexPanelsImageGallery/script.js
@@ -0,0 +1,11 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open')
+} 
+function toggleOpenActive() {
+    this.classList.toggle('open-active');
+}
+
+panels.forEach((panel)  => panel.addEventListener('click',toggleOpen))
+panels.forEach((panel) => panel.addEventListener('click',toggleOpenActive))
